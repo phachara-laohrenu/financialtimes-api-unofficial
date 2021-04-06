c4a4f830ee2922bc31e9380f48bce242bba57494
Phachara Laohrenu <thepoch@Phacharas-MacBook-Pro.local>
2021-04-06 14:31:13 +0700

    update

--- build/lib/ftscraper/api.py
+++ build/lib/ftscraper/api.py
@@ -4,6 +4,8 @@ import numpy as np
 from bs4 import BeautifulSoup
 from datetime import datetime, date
 import operator
+import re
+import json
 
 from ftscraper.utils import similar, etf_or_fund, filter_by_currency
 from ftscraper.search_obj import SearchObj
@@ -174,4 +176,79 @@ def search_select_fund(query, country=None, asset_class=None, income_treatment='
     search_results = search(query)
     pick = select_fund(search_results)
     return pick
-            
+
+def get_summary(sec_name, symbol, sec_type=None):
+    """
+    Get summary of the fund
+    """
+    
+    url = 'https://markets.ft.com/data/funds/tearsheet/summary'
+    payload = {'s': symbol}
+    page = requests.get(url, params=payload)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    if sec_name is None:
+        sec_name = soup.find(class_='mod-tearsheet-overview__header__name mod-tearsheet-overview__header__name--small').text
+
+    profile = soup.find(class_='mod-ui-table mod-ui-table--two-column mod-profile-and-investment-app__table--profile')
+    atts = {'sec_name':sec_name,'sec_type':sec_type,'symbol':symbol}
+    atts['income treatment'] = None
+    for att in profile.findAll('tr'):
+        key = att.find('th').text.lower()
+        if key == 'symbol':
+            continue
+        value = att.find('td').text
+        atts[key] = value
+    
+    atts['xid'] = None
+
+    return atts
+
+def get_xid(symbol):
+    """
+    Get ID of the fund (This ID is unique for ft.com)
+    """
+    url = 'https://markets.ft.com/data/funds/tearsheet/charts'
+    payload = {'s': symbol}
+    page = requests.get(url, params=payload)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = soup.find(class_="mod-app clearfix mod-ichart")
+    xid = re.search('&quot;baseXid&quot;:&quot;(.*)&quot;,&quot;days&quot;', str(soup)).group(1)
+
+    return xid
+
+def get_historical(xid=None, symbol=None):
+    
+    if xid is None:
+        xid = get_xid(symbol)
+    
+    """
+    Get historical prices of the fund
+    """
+    d0 = date(1970, 1, 1)
+    d1 = date.today()
+    delta = d1 - d0
+
+    headers = {
+        'content-type': 'application/json',
+    }
+    payload = {"days":delta.days,
+            "dataNormalized":'false',
+            "dataPeriod":"Day",
+            "returnDateType":"ISO8601",
+            "elements":[{
+                        "Type":"price",
+                        "Symbol":xid,
+                        }]
+            }
+    response = requests.post('https://markets.ft.com/data/chartapi/series', headers=headers, data=json.dumps(payload))
+    response = response.json()
+    dates = response['Dates']
+    dates = [date.split('T')[0] for date in dates]
+    dates = {'date':dates}
+    component_series = response['Elements'][0]['ComponentSeries']
+    prices = {component['Type'].lower(): component['Values'] for component in component_series}
+
+    result = {**dates,**prices}
+    df = pd.DataFrame(result)
+
+    return df
